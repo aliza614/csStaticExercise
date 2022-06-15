d50897c8cda88cbbe37e4b39c1883fb1ff6f5b17
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-06-15 10:25:20 -0500

    initial commit

---
+++ csStaticExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var far1 = 98.8;
+            var cel2 = 32.3;
+
+            var far2=TempConverter.CelsiusToFarenheit(cel2);
+            var cel1 = TempConverter.FarenheitToCelsius(far1);
+
+            Console.WriteLine($"{far1:N2} F is { cel1:N2} C and {cel2:N2} C is {far2:N2} F" );
+        }
+    }
+}
---
+++ csStaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csStaticExercise
+{
+    public static class TempConverter
+    //The class should have at least 2 methods, one called FahrenheitToCelsius that will require a double as a parameter and return a double,
+    //the other CelsiusToFahrenheit which will also require a double as a parameter and return a double.
+    {
+        public static double FarenheitToCelsius(double far)
+        {
+         //   Console.WriteLine($"far - 32.0={far - 32.0}");
+            return (far-32.0)*0.5556;
+        }
+        public static double CelsiusToFarenheit(double cel)
+        {
+            return (cel/ 0.5556)+32;
+        }
+    }
+}
